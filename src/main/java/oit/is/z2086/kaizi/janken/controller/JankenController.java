8a9a655ff9f17a8d89ec1d937b883ea0769ca0f7
e1b21013 <e1b21013@st.oit.ac.jp>
2023-10-09 15:29:03 +0900

    じゃんけんゲーム完成

--- src/main/java/oit/is/z2086/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2086/kaizi/janken/controller/JankenController.java
@@ -4,25 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2086.kaizi.janken.model.Janken;
+
 @Controller
-@RequestMapping("/janken")
 public class JankenController {
   @GetMapping("/janken")
-  public String Janken() {
+  public String janken_get() {
+    return "janken.html";
+  }
+
+  @PostMapping("/janken")
+  public String janken_post(@RequestParam String name1, ModelMap model) {
+    model.addAttribute("jankenpost", name1);
     return "janken.html";
   }
 
   /**
    *
    */
-  @PostMapping("/janken")
-  public String janken(@RequestParam Integer name1, ModelMap model) {
-    model.addAttribute("name1", name1);
+  @GetMapping("/jankengame")
+  public String jankenGame(@RequestParam String hand, ModelMap model1,ModelMap model2) {
+    Janken jn = new Janken(hand);
+    model1.addAttribute("hand", hand);
+    model2.addAttribute("jankenResult", jn.getResult());
     return "janken.html";
   }
-
 
 }
--- src/main/java/oit/is/z2086/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z2086/kaizi/janken/model/Janken.java
@@ -1,5 +1,23 @@
 package oit.is.z2086.kaizi.janken.model;
 
 public class Janken {
-  
+  String Result;
+
+  public Janken(String hand) {
+    if (hand.equals("Gu")) {
+      Result = "Aiko";
+    } else if (hand.equals("Pa")) {
+      Result = "You Win!";
+    } else {
+      Result = "You lose";
+    }
+  }
+
+  public String getResult() {
+    return Result;
+  }
+
+  public void setAve(String Result) {
+    this.Result = Result;
+  }
 }
