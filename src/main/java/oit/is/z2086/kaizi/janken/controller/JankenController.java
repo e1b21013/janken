98e56bb6102181726658380608e1d8ebab1fe256
e1b21013 <e1b21013@st.oit.ac.jp>
2023-10-23 13:38:31 +0900

    ユーザ情報をSELECTで取得して表示する

--- src/main/java/oit/is/z2086/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2086/kaizi/janken/controller/JankenController.java
@@ -1,8 +1,7 @@
 package oit.is.z2086.kaizi.janken.controller;
 
 import java.security.Principal;
-
-
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,32 +12,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z2086.kaizi.janken.model.Janken;
 import oit.is.z2086.kaizi.janken.model.Entry;
+import oit.is.z2086.kaizi.janken.model.UserMapper;
+import oit.is.z2086.kaizi.janken.model.User;
 
 @Controller
 public class JankenController {
 
   @Autowired
   private Entry entry;
+  @Autowired
+  UserMapper userMapper;
+
   @GetMapping("/janken")
-  public String janken_get(ModelMap model, Principal prin) {
+  public String janken_get(ModelMap model,Principal prin) {
     String loginUser = prin.getName();
     this.entry.addUser(loginUser);
     model.addAttribute("login_user", loginUser);
-    model.addAttribute("entry", this.entry);
+    ArrayList<User> users = userMapper.selectAllUsers();
+    model.addAttribute("users", users);
+    // model.addAttribute("entry", this.entry);
     return "janken.html";
   }
 
-  /*@PostMapping("/janken")
-  public String janken_post(@RequestParam String name1, ModelMap model) {
-    model.addAttribute("jankenpost", name1);
-    return "janken.html";
-  }*/
+  /*
+   * @PostMapping("/janken")
+   * public String janken_post(@RequestParam String name1, ModelMap model) {
+   * model.addAttribute("jankenpost", name1);
+   * return "janken.html";
+   * }
+   */
 
   /**
    *
    */
   @GetMapping("/jankengame")
-  public String jankenGame(@RequestParam String hand, ModelMap model1,ModelMap model2) {
+  public String jankenGame(@RequestParam String hand, ModelMap model1, ModelMap model2) {
     Janken jn = new Janken(hand);
     model1.addAttribute("hand", hand);
     model2.addAttribute("jankenResult", jn.getResult());
